dbe884c61e4bce32b67aa1957d8cba15314609e6
Scott Matsumoto Hirabayashi <shiraba1@umbc.edu>
2021-10-24 23:36:10 -0400

    Update SQL_TRACKER_PRACTICE.sql

--- SQL_PRACTICE/SQL_TRACKER_PRACTICE.sql
+++ SQL_PRACTICE/SQL_TRACKER_PRACTICE.sql
@@ -1,3 +1,38 @@
+'''
+DEFINE TABLE employee(report_from: INT, report_to: INT)
+
+Employee | Manager
+Report_from|  Report_to
+1		2  == 11,12,13
+2		3
+3		8
+7		8
+5		7
+10		7
+9		10
+11		1
+12		11
+13		11
+14		12
+
+1-2-3-8
+2-3-8
+3-8-
+7-8
+5-7-8
+10-7-8
+11-1-2-3-8
+12-11-1-2-3-8
+13-11-1-2-3-8
+14-12-11-1-2-3-8
+
+For each individual, find the total number of employees that report to them. (including direct report and all the indirect reports)
+Example, there is 1 employee directly reporting to 1 (#11), but indirectly, #12,#13 and #14 all report to 1. Therefore, #1 should have 1+3 = 4 reports.
+Do this for all employees, and if one doesn’t have a direct report, the count is 0.
+Do this in SQL and Pandas.
+
+'''
+
 CREATE TABLE if not exists employees (
   report_from integer,
   report_to integer
