299502acb8878528b24fa2158acd5255c2fd9511
Scott Matsumoto Hirabayashi <shiraba1@umbc.edu>
2021-10-24 23:32:02 -0400

    Update SQL_Tracker_Practice_1.sql

--- SQL_Tracker_Practice_1.sql
+++ SQL_Tracker_Practice_1.sql
@@ -13,3 +13,24 @@ SELECT AVG(score) AS ave_score
 FROM scores 
 WHERE student_id % 2 = 0 
 GROUP BY subject
+
+'''
+
+Followup: we don’t observe student_id, each individual is ordered within each subject
+
+Generate your own student_id
+
+Student  Subject    student_id
+
+A             Math          1
+B             Math          2
+A            English       1
+B            English       2
+'''
+
+WITH score_id AS 
+  (SELECT *, ROW_NUMBER() OVER (PARTITION BY subject) student_id 
+     FROM scores)
+
+SELECT subject, AVG(score) score FROM score_id
+     WHERE ( student_id % 2 ) = 0 GROUP BY subject;
